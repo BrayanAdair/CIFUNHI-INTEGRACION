74de0d687f7513db7cf0ddb4573d5e6352e32710
BrayanAdair <brayanadairmontano083@gmail.com>
2023-02-16 18:56:33 -0600

    Arbol de Directorios
    By: Brayan Modificacion del arbol del directorios

---
+++ js/scripts/login.js
@@ -0,0 +1,10 @@
+// ocultar y activar alerta de olvidar constraseña 
+let ocultar = document.querySelector(".alert__forgetPassword");
+function alerVisible(){
+    ocultar.style.visibility = "visible";
+
+}
+function alerHidden(){
+    ocultar.style.visibility = "hidden";
+
+}
